9bea660e68faef8a180ec84c3912eb6127a75328
Evan Rispoli <evan.rispoli@gmail.com>
2022-12-10 12:06:25 -0500

    Initial commit

---
+++ app/src/main/java/devkotlin/evanrispoli/myapplication/EmployeeModel.kt
@@ -0,0 +1,9 @@
+package devkotlin.evanrispoli.myapplication
+
+class EmployeeModel(
+    var empId: String? = null,
+    var empName: String? = null,
+    var empAge: String? = null,
+    var empSalary: String? = null) {
+
+}
